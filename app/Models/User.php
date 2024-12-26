0a778efeac712d637502846ed7585ca589aee7d0
azizovmuhammadali <alico.777777@gmail.com>
2024-12-26 16:01:05 +0500

    I've changed User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Models;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -53,5 +51,9 @@ class User extends Authenticatable
     }
     public function comments(){
         return $this->hasMany(Comment::class);
+    }
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
     }
 }

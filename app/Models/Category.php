6c5e2a27387d770876551151cf7b267b570cf143
azizovmuhammadali <alico.777777@gmail.com>
2024-12-26 16:00:25 +0500

    I've changed some places

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,22 +5,20 @@ namespace App\Models;
 use App\Observers\CategoryObserver;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 #[ObservedBy(CategoryObserver::class)]
 class Category extends Model
 {
     protected $fillable = [
         "title",
-        'image',
         'slug',
     ];
     public function phones()
     {
         return $this->hasMany(Phone::class);
     }
-    public function url(): Attribute
+    public function images()
     {
-        return Attribute::make(fn(): string => URL::to('storage/' . $this->image));
+        return $this->morphMany(Image::class, 'imageable');
     }
 }

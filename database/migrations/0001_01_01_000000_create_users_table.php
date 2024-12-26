1b2481a60718eafb6eda8c7dcff7bb3b350ffbc4
azizovmuhammadali <alico.777777@gmail.com>
2024-12-26 16:01:21 +0500

    I've updated user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('verification_token');
             $table->string('phone_number');
-            $table->string('role')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();

35d2345902a75e40a978fcecc78376448dff9cc8
azizovmuhammadali <alico.777777@gmail.com>
2024-12-27 12:01:02 +0500

    Reuirement of Comments

--- app/Http/Requests/CommentStoreRequest.php
+++ app/Http/Requests/CommentStoreRequest.php
@@ -22,7 +22,7 @@ class CommentStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-           'name' => 'required',
+           'comment' => 'required',
         ];
     }
 }

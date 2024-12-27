31c21707eacbfff55293fde80ccac69f953a373c
azizovmuhammadali <alico.777777@gmail.com>
2024-12-27 12:02:39 +0500

    CategoryResource

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -15,9 +15,8 @@ class CategoryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "id"=> $this->id,
             "title"=> $this->title,
-            "image"=> $this->url,
+            "images"=> ImageResource::collection($this->whenLoaded("images")),
         ];
     }
 }

fc9f5e2009291181ebbc5a230ce4df83e031cb39
azizovmuhammadali <alico.777777@gmail.com>
2024-12-27 12:04:57 +0500

    It's connection between Observer and Models

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Phone;
 use App\Models\Category;
+use App\Observers\PhoneObserver;
 use App\Observers\CategoryObserver;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,5 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Category::observe(CategoryObserver::class);
+        Phone::observe(PhoneObserver::class);
     }
 }
